abbe9454116336f0a1c0c0cb167ff653f7b5fce3
rluca6448 <rluca6448@gmail.com>
2024-05-24 11:25:05 -0300

    Comiteo los archivos base

---
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -0,0 +1,19 @@
+/* sampleCodeModule.c */
+
+#include <stdint.h>
+
+char * v = (char*) 0xB8000 + 79 * 2;
+
+static int var1 = 0;
+static int var2 = 0;
+
+int main() {
+	*v = 'X';
+	*(v+1) = 0x74;
+
+	//Test if BSS is properly set up
+	if (var1 == 0 && var2 == 0)
+		return 0xDEADC0DE;
+
+	return 0xDEADBEEF;
+}

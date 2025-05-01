2566d643cb7d9e8be422adcbe28f658fc1ee7eb2
maximocitba <mcarranza@itba.edu.ar>
2025-05-01 20:45:31 -0300

    Remove obsolete compilation scripts and consolidate Docker execution into a single run.sh file

--- run.sh
+++ run.sh
@@ -33,9 +33,3 @@ fi
 
 # para cerrar:
 docker rm $NOMBRE
-
-# ni idea qué es esto:
-# export GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"
-# export LS_COLORS="rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:m"
-# alias ls='ls --color=auto'
-# cd /root
